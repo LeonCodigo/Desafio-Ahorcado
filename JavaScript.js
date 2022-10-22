d130a8ed0527b4ab3fe5fa708b10950adf9eebec
Leon <leongermdp@gmail.com>
2022-10-22 01:30:43 -0300

    Primeros archivos

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+﻿
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+var btn_iniciar = document.getElementById("btn_iniciar");
+
+function iniciar()
+{
+  //  pantalla.hidden;
+    alert("hola");
+}
+
+btn_iniciar.onclick = iniciar;
