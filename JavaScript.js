7fdb6b235a0df3d9372708edc4df1ec95e9c29b4
Leon <leongermdp@gmail.com>
2022-10-30 02:16:47 -0300

    Se agrego dibujo de ahorcado y coordinacion de dibujado

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,4 @@
-﻿var palabraSecreta = "null",letrasCorrectas=[],letrasIncorrectas=[];
+﻿var palabraSecreta = "null", letrasCorrectas = [], letrasIncorrectas = [];
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 //botones
@@ -13,20 +13,19 @@ var btn_cancelaAgregarP = document.getElementById("btn_cancelarAgregar");
 
 // Carteles
 var cartel_ganar = document.getElementById("cartelGanaste");
-var cartel_perder = document.getElementById("cartelPerdiste"); 
+var cartel_perder = document.getElementById("cartelPerdiste");
 var textoPalabraCorrecta = document.getElementById("palabraCorrecta");
 var cartel_nuevaPalabra = document.getElementById("cartelNuevaPalabra");
 
 var palabraIngresada = document.getElementById("inputPalabra");
 //-----
 var tecladoHabilitado = false;
-var palabras = ["TECLADO", "RATON", "MICROFONO", "MONITOR", "COMPUTADORA", "USB", "CAMARA", "PENDRIVE", "WIFI", "CPU","ESCRITORIO","TELEVISOR"];
-var separacion = 35,inicioX=500,inicioY=400;
+var palabras = ["TECLADO", "RATON", "MICROFONO", "MONITOR", "COMPUTADORA", "USB", "CAMARA", "PENDRIVE", "WIFI", "CPU", "ESCRITORIO", "TELEVISOR"];
+var separacion = 35, inicioX = 500, inicioY = 400;
 
 
 
-function numeroAleatorio(pMax)
-{
+function numeroAleatorio(pMax) {
     let res = 0, min = 0;
     pMax--;
     res = Math.floor(Math.random() * (pMax - min + 1) + min)
@@ -35,7 +34,7 @@ function numeroAleatorio(pMax)
 
 function crear_lineas(posX, cant) {
 
-    pincel.clearRect(0, 0, 1280,860);
+    pincel.clearRect(0, 0, 1280, 860);
     let i = 0;
     while (i < cant) {
         pincel.fillStyle = "blue"
@@ -52,26 +51,22 @@ function crear_palabra_secreta() //funciona
     aleatorio = numeroAleatorio(palabras.length);
     if (aleatorio < palabras.length) {
         res = palabras[aleatorio];
-    }    
+    }
     return res;
 }
 
-function escribe_letra(pLetra, pX, pY)
-{
+function escribe_letra(pLetra, pX, pY) {
     var pantalla = document.querySelector("canvas");
     var pincel = pantalla.getContext("2d");
-    pincel.font ="35px Lobster";
+    pincel.font = "35px Lobster";
     pincel.fillStyle = "blue";
     pincel.fillText(pLetra, pX, pY);
 }
 
-function ubica_letras(pLetra)
-{
-    for (let i = 0; i < palabraSecreta.length; i++)
-    {
-        if (pLetra === palabraSecreta[i])
-        {
-            escribe_letra(pLetra, inicioX + separacion * i, inicioY -20);
+function ubica_letras(pLetra) {
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (pLetra === palabraSecreta[i]) {
+            escribe_letra(pLetra, inicioX + separacion * i, inicioY - 20);
             letrasCorrectas.push(pLetra);
         }
     }
@@ -86,42 +81,34 @@ function escribe_resultado(pResultado, pX, pY) {
 }
 
 
-function verificador(e)
-{
+function verificador(e) {
     res = "null";
-    if (e.key.match("[A-Z\s]") !== null) {
-        if (e.key != "Enter")
-        {
+    if (e.key.match(/[A-Z\s]/) !== null) {
+        if (e.key != "Enter") {
             res = e.key;
-        }     
+        }
     }
     return res;
 }
 
 
-function letra_Correcta(e)
-{
-    
-    palabraSecreta.lastIndexOf
-   
-    let letra= "null",pos=0;
+function letra_Correcta(e) {
+
+    let letra = "null", pos = 0;
     letra = verificador(e);
-    if (letra !== "null")
-    {
-        if (palabraSecreta.includes(letra) && !letrasCorrectas.includes(letra))
-        {
+    if (letra !== "null") {
+        if (palabraSecreta.includes(letra) && !letrasCorrectas.includes(letra)) {
             ubica_letras(letra);
         }
-        else if (!letrasIncorrectas.includes(letra) && !letrasCorrectas.includes(letra))
-        {             
-            escribe_letra(letra, inicioX + separacion * letrasIncorrectas.length, inicioY+80);
-                letrasIncorrectas.push(letra);
-        }        
+        else if (!letrasIncorrectas.includes(letra) && !letrasCorrectas.includes(letra)) {
+            escribe_letra(letra, inicioX + separacion * letrasIncorrectas.length, inicioY + 80);
+            letrasIncorrectas.push(letra);
+            dibuja_hombrecito(letrasIncorrectas);
+        }
     }
     else { alert("Solo se puede jugar con letras en MAYUSCULAS.") }
 
-    if (palabraSecreta.length === letrasCorrectas.length)
-    {
+    if (palabraSecreta.length === letrasCorrectas.length) {
         cartel_ganar.style.display = "block";
         document.removeEventListener("keypress", letra_Correcta);
     }
@@ -132,13 +119,11 @@ function letra_Correcta(e)
     }
 }
 
-function dibujaMiembros(pX,pY,finX,finY,pGrosor)
-{
-  //podria ir coordinando con letras_acertadas.lenght. para cada instancia del dibujo
+function dibujaMiembros(pX, pY, finX, finY, pGrosor, pColor) {
     //intentar hacer la base cuadrada y el tipito redondo. 
     pincel.lineWidth = pGrosor;
-    pincel.fillStyle = "blue";  
-    pincel.strokeStyle = "#e7c874"
+    pincel.fillStyle = "blue";
+    pincel.strokeStyle = pColor
     pincel.lineCap = "round";
     pincel.lineJoin = "round";
     pincel.beginPath();
@@ -163,45 +148,70 @@ function dibujaPalos(pX, pY, finX, finY, pGrosor) {
     pincel.closePath();
 }
 
-function dibuja_parapeto()
-{
+function dibuja_parapeto() {
     dibujaPalos(inicioX, inicioY - 60, 800, inicioY - 60, 15); // BASE
-    dibujaPalos(inicioX + 150, inicioY - 65, inicioX + 150, (inicioY-75-280), 15); // MASTIL
-    dibujaPalos(inicioX + 150, (inicioY - 75 - 280), 900, (inicioY - 75 - 280) , 9); // BRAZO
+    dibujaPalos(inicioX + 150, inicioY - 65, inicioX + 150, (inicioY - 75 - 280), 15); // MASTIL
+    dibujaPalos(inicioX + 150, (inicioY - 75 - 280), 900, (inicioY - 75 - 280), 9); // BRAZO
 }
 
-function mostrar_pantalla_inicial(bool)
+function dibuja_circulo() {
+    pincel.fillStyle = "#e7c874";
+    pincel.arc(900, 105, 20, 0, 2 * 3.14);
+    pincel.fill();
+}
+
+
+
+function dibuja_hombrecito(pArreglo)
 {
+    // control de estado del hombre con palabras_incorrectas.length
+    //cabeza 
+
+    if (pArreglo.length === 0) { dibujaMiembros(900, 50, 900, 83, 5, "#ff7d00"); }//soga 
+
+    if (pArreglo.length === 1) { dibujaMiembros(900, 50, 900, 83, 5, "#ff7d00"); dibuja_circulo(); }
+
+    if (pArreglo.length === 2) {
+        dibujaMiembros(900, 120, 900, 220, 15, "#e7c874"); }//torzo
+    if (pArreglo.length === 3) {
+        dibujaMiembros(900, 125, 870, 170, 10, "#e7c874");
+    }// brazo izquierdo
+
+    if (pArreglo.length === 4) { dibujaMiembros(900, 125, 930, 170, 10, "#e7c874"); }
+    // brazo der
+
+    if (pArreglo.length === 5) { dibujaMiembros(900, 210, 870, 260, 12, "#e7c874"); }// pierna izq
+    if (pArreglo.length === 6) { dibujaMiembros(900, 210, 930, 260, 12, "#e7c874"); dibujaMiembros(893, 125, 907, 125, 5, "#ff7d00"); } // pierna der y soga cuello   
+}
+
+function mostrar_pantalla_inicial(bool) {
     let ubicacion = "none";
-    if (bool) { ubicacion = "block";}
+    if (bool) { ubicacion = "block"; }
 
     pincel.clearRect(0, 0, 1280, 860);
     btn_iniciar.style.display = ubicacion;
-    btn_agregarInicio.style.display = ubicacion;    
+    btn_agregarInicio.style.display = ubicacion;
 }
 
-function mostrar_pantalla_juego(bool)
-{   
+function mostrar_pantalla_juego(bool) {
     let ubicacion = "none";
     if (bool) { ubicacion = "inline"; }
     btn_nuevoJuego.style.display = ubicacion;
     btn_desistir.style.display = ubicacion;
 }
 
-    
 
-function mostrar_pantalla_agregarPalabra(Bool)
-{
+
+function mostrar_pantalla_agregarPalabra(Bool) {
     let ubicacion = "none";
     if (Bool) { ubicacion = "block"; }
 
     btn_agregarPalabra.style.display = ubicacion;
-    btn_cancelaAgregarP.style.display = ubicacion;  
+    btn_cancelaAgregarP.style.display = ubicacion;
 
 }
 
-function iniciar_pantalla_inicial()
-{
+function iniciar_pantalla_inicial() {
     document.removeEventListener("keypress", letra_Correcta);
     mostrar_pantalla_inicial(true);
     mostrar_pantalla_juego(false)
@@ -209,37 +219,35 @@ function iniciar_pantalla_inicial()
 }
 
 function fun_AgregarPalabra() {
+    cartel_nuevaPalabra.style.display = "block";
     if (palabraIngresada.value != "") {
         palabras.push(palabraIngresada.value);
         palabraIngresada.value = "";
-        cartel_nuevaPalabra.style.display = "none"; juego();
+        cartel_nuevaPalabra.style.display = "none";
+        juego();
     }
     else { alert("Debe ingresar una palabra"); }
-    console.log(palabras);
 }
 
-
-function juego()
-{
+function juego() {
     mostrar_pantalla_juego(true);
     mostrar_pantalla_inicial(false);
     letrasCorrectas = [];
     letrasIncorrectas = [];
     palabraSecreta = crear_palabra_secreta();
-    alert(palabraSecreta);
     textoPalabraCorrecta.textContent = palabraSecreta;
     crear_lineas(inicioX, palabraSecreta.length);
     document.addEventListener("keypress", letra_Correcta);
     dibuja_parapeto();
-    letra_Correcta();   
+    letra_Correcta();
 }
 
 btn_iniciar.onclick = juego;
 btn_nuevoJuego.onclick = juego;
 btn_desistir.onclick = iniciar_pantalla_inicial;
-btn_aceptarG.onclick = function() { cartel_ganar.style.display = "none"; }
+btn_aceptarG.onclick = function () { cartel_ganar.style.display = "none"; }
 btn_aceptarP.onclick = function () { cartel_perder.style.display = "none"; }
 btn_agregarPalabra.onclick = fun_AgregarPalabra;
 
 btn_cancelaAgregarP.onclick = function () { cartel_nuevaPalabra.style.display = "none"; iniciar_pantalla_inicial(); }
-btn_agregarInicio.onclick = function () { mostrar_pantalla_inicial(false); cartel_nuevaPalabra.style.display = "block"; }
+btn_agregarInicio.onclick = function () { mostrar_pantalla_inicial(false); cartel_nuevaPalabra.style.display = "block"; mostrar_pantalla_agregarPalabra(true); }
